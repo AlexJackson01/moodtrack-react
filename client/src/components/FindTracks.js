3d1c879f4bba2dd194555d00601463a658e9bce2
AlexJackson01 <ajjackson22@gmail.com>
2022-07-20 14:22:40 +0100

    Added Material UI sliders to save user mood inputs

--- client/src/components/FindTracks.js
+++ client/src/components/FindTracks.js
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Logo from '../images/MoodTrack_logo.png';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
 export default function FindTracks({ token, setToken }) {
 
     const [trackList, setTrackList] = useState([]);
+    const [showTrack, setShowTrack] = useState(false);
+    const [dance, setDance] = useState("");
+    const [energy, setEnergy] = useState("");
+    const [valence, setValence] = useState(""); 
 
     const logout = (e) => {
         e.preventDefault();
@@ -80,11 +86,71 @@ export default function FindTracks({ token, setToken }) {
     //     findTracks();
     // }, [token])
 
+    const findRecommendation = (e) => {
+  
+    }
+
   return (
     <div>
         <div className='logo-container'>
             <img className='moodtrack-logo-small' src={Logo} alt='moodtrack logo' />
             <button onClick={(e) => logout(e)}>Logout</button>
+
+            <div className='container'>
+                {token && !showTrack ? (
+                    <div>
+                        <h1>How are you feeling today?</h1>
+                        <form onSubmit={(e) => findRecommendation(e)}>
+                            <Box sx={{ margin: '0 auto', width: 600, marginTop: '50px', marginBottom: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <span className='emojis' role="img" aria-label="seat">💺</span>
+                                <Slider
+                                    onChange={(e) => setDance(e.target.value / 10)}
+                                    aria-label="Temperature"
+                                    defaultValue={0}
+                                    step={1}
+                                    marks
+                                    min={0}
+                                    max={10}
+                                    sx={{ color: '#8C52FF', marginLeft: '50px', marginRight: '50px' }}
+                                />
+                                <span className='emojis' role="img" aria-label="dancer">💃</span>
+                            </Box>
+    
+                            <Box sx={{ margin: '0 auto', width: 600, marginTop: '50px', marginBottom: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <span className='emojis' role="img" aria-label="sleep">💤</span>
+                                <Slider
+                                    onChange={(e) => setEnergy(e.target.value / 10)}
+                                    aria-label="Temperature"
+                                    defaultValue={0}
+                                    step={1}
+                                    marks
+                                    min={0}
+                                    max={10}
+                                    sx={{ color: '#8C52FF', marginLeft: '50px', marginRight: '50px' }}
+                                />
+                                <span className='emojis' role="img" aria-label="sports">🏂</span>
+                            </Box>
+    
+                            <Box sx={{ margin: '0 auto', width: 600, marginTop: '50px', marginBottom: '20px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                                <span className='emojis' role="img" aria-label="happy">😄</span>
+                                <Slider
+                                    onChange={(e) => setValence(e.target.value / 10)}
+                                    aria-label="Temperature"
+                                    defaultValue={0}
+                                    step={1}
+                                    marks
+                                    min={0}
+                                    max={10}
+                                    sx={{ color: '#8C52FF', marginLeft: '50px', marginRight: '50px' }}
+                                />
+                                <span className='emojis' role="img" aria-label="crying">😭</span>
+                            </Box>
+    
+                            <button type='submit' className='search-button'>Get Today's MoodTrack</button>
+                        </form>
+                    </div>
+            ) : null}
+            </div>
         </div>
     </div>
   )
